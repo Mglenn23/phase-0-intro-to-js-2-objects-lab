39c3b4663cd88743efdbdb9c1672ea016ef07ea2
michael <michaelglenn88@yahoo.com>
2022-03-16 14:39:48 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {};
+employee.name = "Mich";
+employee["streetAddress"] = "Citra Garden 2";
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+  const cloneEmployee = { ...obj };
+  cloneEmployee[key] = value;
+  return cloneEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, val) {
+  obj[key] = val;
+  return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key, val) {
+  const cloneEmployee = { ...obj };
+  cloneEmployee[key] = val;
+  delete cloneEmployee.key;
+  return cloneEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key, val) {
+  delete obj[key];
+  return obj;
+}
